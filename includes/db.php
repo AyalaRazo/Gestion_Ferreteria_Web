baf02f77cab5d764b0dfe1290affadd915de49ef
AyalaRazo <ayalarazojulio13@gmail.com>
2025-09-27 23:50:59 -0700

    Proyecto Base

--- includes/db.php
+++ includes/db.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $dbname = 'proyecto';
 $username = 'root';
-$password = 'ItzJulio_123';
+$password = 'admin';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
